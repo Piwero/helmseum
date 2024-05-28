988dfeb5c396b55deb7535e0d79c48b12a825d7d
Piwero <piwero@gmail.com>
2024-05-28 07:43:55 +0100

    Revert "fix: Try new workflow"
    This reverts commit acece1782a24547f89e45516e4f6e856c71ac744.

--- .releaserc.js
+++ .releaserc.js
@@ -1,36 +1,11 @@
-const yaml = require('js-yaml');
-const Handlebars = require('handlebars')
-
-Handlebars.registerHelper("yamlString", function (options) {
-    const input = options.fn(this).trim()
-    return yaml.dump(input.replace(/^\s*\*\s*/g, '').trim(), {lineWith: 999, forceQuotes: true, quotingType: '"'});
-});
-
-/*
-const chartPath = process.env.CHART_PATH;
-if (!chartPath) {
-  throw new Error('No chart path defined')
-}
-*/
 
 module.exports = {
     "branches": [
-        "main",
+        "main"
     ],
-    "tagFormat": process.env.CHART_NAME + "-v${version}",
     "plugins": [
-        [
-            "@semantic-release/commit-analyzer",
-            {}
-        ],
-        [
-            "@semantic-release/release-notes-generator",
-            {
-                "writerOpts": {
-                    "mainTemplate": "{{#each commitGroups}}{{#each commits}} - {{#yamlString}}{{> commit root=@root}}{{/yamlString}}{{/each}}{{/each}}",
-                }
-            }
-        ],
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
         [
             "@semantic-release/changelog",
             {
@@ -38,27 +13,10 @@ module.exports = {
             }
         ],
         [
-            "@semantic-release/exec",
+            'semantic-release-helm3',
             {
-                "prepareCmd": "helm-docs",
-                "publishCmd": "cr package . && cr upload -o helmseum -r helm-charts -c $(git rev-parse HEAD) --release-notes-file CHANGELOG.md --push --packages-with-index --skip-existing"
-            }
-        ],
-        [
-            "semantic-release-helm3",
-            {
-                "chartPath": ".",
-                "populateChangelog": "true",
-                "onlyUpdateVersion": "true"
-            }
-        ],
-        [
-            "@semantic-release/git",
-            {
-                "assets": [
-                    "Chart.yaml"
-                ],
-                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+                chartPath: 'charts/test-1',
+                onlyUpdateVersion: true,
             }
         ]
     ]
